359b5f80e75915b889bc7524a9162a2c143f2852
shitanshu-277 <shitanshugoyal2002@gmail.com>
2024-10-28 18:33:21 +0000

    redis credentials updated

--- Backend/redis_connection.js
+++ Backend/redis_connection.js
@@ -1,6 +1,13 @@
 const redis = require("redis");
+require('dotenv').config();
 const RedisStore = require("connect-redis").default;
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT
+    }
+});
 
 redisClient.connect().catch(console.error);
 
